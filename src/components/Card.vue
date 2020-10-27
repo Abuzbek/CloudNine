bbe170e51da88f1fba333f4a8628d5f0b08b4513
donyBro <doniyorrahmanov957@mail.ru>
2020-10-27 07:49:04 +0500

    installed vuetify , card component is done, form component is in process

---
+++ src/components/Card.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="wrapper">
+    <p class="title">{{ title }}</p>
+    <img class="logo" :src="logo" alt="logo" />
+    <p class="body">{{ body }}</p>
+    <p class="price">От {{ price }} $</p>
+    <button class="btn">
+      Заказать
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: '',
+      },
+      logo: {
+        type: String,
+        default: '',
+      },
+      body: {
+        type: String,
+        default: '',
+      },
+      price: {
+        type: String,
+        default: '',
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    background: #ffffff;
+    border-radius: 10px;
+    padding: 3%;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  }
+  .title {
+    margin: 0 auto;
+    margin-bottom: 15%;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 32px;
+  }
+  .logo {
+    width: 100px;
+    height: 100px;
+    margin: 0 auto;
+    margin-bottom: 15%;
+  }
+  .body {
+    width: 100%;
+  }
+  .price {
+    font-size: 24px;
+    line-height: 32px;
+  }
+  .btn {
+    background: #fff;
+    cursor: pointer;
+    padding: 5%;
+    outline: none;
+    margin: 0 auto;
+    border-radius: 30px;
+    color: #2824ea;
+    border: 1px solid #2824ea;
+    width: 75%;
+    font-size: 18px;
+    line-height: 24px;
+  }
+  .btn:hover {
+    color: #fff;
+    background: #2824ea;
+  }
+</style>
---
+++ src/components/WriteUs.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper ma-5">
+    <div class="text-center text-h6 mt-5 font-weight-bold">Напишите нам</div>
+    <v-text-field
+      label="Main input"
+      :rules="rules"
+      hide-details="auto"
+      class="mx-16"
+    ></v-text-field>
+    <v-text-field label="Another input" class="mx-16"></v-text-field>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'WriteUs',
+    data: () => ({
+      rules: [
+        (value) => !!value || 'Required.',
+        (value) => (value && value.length >= 3) || 'Min 3 characters',
+      ],
+    }),
+  };
+</script>
+
+<style scoped>
+  .wrapper {
+    background: linear-gradient(
+      180deg,
+      rgba(224, 223, 255, 0.8) 0%,
+      rgba(236, 236, 255, 0.5) 100%
+    );
+    width: 50%;
+  }
+</style>
